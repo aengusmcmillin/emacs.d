6d13d61c9200bf44a24ccf24f2840f203de97882
Aengus McMillin <aengusmcmillin@gmail.com>
2018-05-26 15:13:09 -0700

    Few more tweaks

--- custom-settings.el
+++ custom-settings.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("9d9fda57c476672acd8c6efeb9dc801abea906634575ad2c7688d055878e69d6" default))))
+    ("9d9fda57c476672acd8c6efeb9dc801abea906634575ad2c7688d055878e69d6" default)))
+ '(package-selected-packages
+   (quote
+    (rust-mode doom-themes magit general which-key projectile evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
