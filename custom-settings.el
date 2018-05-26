8e924db7432b93fdd2d86d832a29cabe35a9f7f7
Aengus McMillin <aengusmcmillin@gmail.com>
2018-05-26 11:12:46 -0700

    Fixup various things

--- custom-settings.el
+++ custom-settings.el
@@ -3,9 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
+ '(custom-safe-themes
    (quote
-    (auto-compile evil-collection evil-magit general doom-themes delight which-key use-package try projectile org-bullets org-brain magit-lfs iedit helm gruvbox-theme evil-surround evil-leader evil-indent-textobject))))
+    ("9d9fda57c476672acd8c6efeb9dc801abea906634575ad2c7688d055878e69d6" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
